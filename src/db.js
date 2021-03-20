d400532e4beb53b1e8caf5cac7cb75d1ce4027ed
SaturnM <mh5.4wasugi10comaru@gmail.com>
2021-03-20 20:41:07 +0900

    added firebase config

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const config = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID
+}
+
+const db = firebase.initializeApp(config);
+export default db;
